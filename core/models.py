c556bbd2aed4803153bacb86d1bb3c27ec9515b7
Munshi Moshihud Hossain <63121629+mmoshihud@users.noreply.github.com>
2023-09-18 13:19:12 +0600

    Custom User and Managers model done

--- core/models.py
+++ core/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext as _
+from .managers import CustomUserManager
 
-# Create your models here.
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_("email address"), unique=True)
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
