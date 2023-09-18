af94b2056e7476fe205d8f2069244af116a4d3ef
Munshi Moshihud Hossain <63121629+mmoshihud@users.noreply.github.com>
2023-09-18 19:02:18 +0600

    Organization user manage

--- core/serializer.py
+++ core/serializer.py
@@ -1,5 +1,7 @@
+from urllib import request
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from core.models import OrganizationUser, User
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -10,3 +12,21 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return get_user_model().objects.create_user(**validated_data)
+
+
+class OrganizationUserSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = OrganizationUser
+        fields = "__all__"
+
+    def create(self, validated_data):
+        user_data = validated_data.pop("user")
+        user, _ = User.objects.get_or_create(
+            email=user_data["email"], defaults=user_data
+        )
+        request = self.context.get("request")
+        user = request.user
+        organization_user = OrganizationUser.objects.create(user=user, **validated_data)
+        return organization_user
