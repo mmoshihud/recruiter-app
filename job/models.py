d1135c508bc317efbf0d2facd1ec9c31f87b7a74
Munshi Moshihud Hossain <18-39125-3@student.aiub.edu>
2023-09-20 01:05:24 +0600

    Offer model created

--- job/models.py
+++ job/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 from django.utils import timezone
-from core.models import Organization
+from core.models import Organization, User
 
 
 class Job(models.Model):
     title = models.CharField(max_length=255)
-    vacancy = models.IntegerField()
+    vacancy = models.PositiveIntegerField()
     location = models.CharField(max_length=255)
-    description = models.TextField()
-    requirements = models.TextField()
+    description = models.TextField(blank=True)
+    requirements = models.TextField(blank=True)
     salary = models.DecimalField(max_digits=10, decimal_places=2)
     posting_date = models.DateTimeField(auto_now=True)
     expiration_date = models.DateTimeField(default=timezone.now)
     organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    job_poster = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
@@ -20,7 +21,16 @@ class Job(models.Model):
 
 class Application(models.Model):
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    applicant = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     resume_url = models.URLField(max_length=200)
     application_date = models.DateTimeField(auto_now=True)
+
+
+class Offer(models.Model):
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    applicant = models.ForeignKey(User, on_delete=models.CASCADE)
+    offer_date = models.DateTimeField(default=timezone.now)
+    offer_accepted = models.BooleanField()
+    start_date = models.DateField()
