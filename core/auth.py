0e36b68c473e1bc946ca3394ea76d910816a1464
Munshi Moshihud Hossain <63121629+mmoshihud@users.noreply.github.com>
2023-09-20 13:28:15 +0600

    auth fix

--- core/auth.py
+++ core/auth.py
@@ -1,20 +1,42 @@
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.authtoken.models import Token
-from rest_framework.response import Response
+from django.contrib.auth import authenticate
+from django.utils.translation import gettext as _
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.response import Response
 
 
 class CustomAuthTokenSerializer(serializers.Serializer):
     email = serializers.EmailField()
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(
+        style={"input_type": "password"},
+        trim_whitespace=False,
+    )
+
+    def validate(self, attr):
+        email = attr.get("email")
+        password = attr.get("password")
+        user = authenticate(
+            request=self.context.get("request"),
+            username=email,
+            password=password,
+        )
+        if not user:
+            message = _("Unable to authenticate with provided credentials.")
+            raise serializers.ValidationError(message, code="authorization")
+
+        attr["user"] = user
+        return attr
 
 
 class CustomAuthToken(ObtainAuthToken):
+    serializer_class = CustomAuthTokenSerializer
+
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(
             data=request.data, context={"request": request}
         )
         serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data.get("user")  # Use get() to avoid KeyError
+        user = serializer.validated_data["user"]
         token, _ = Token.objects.get_or_create(user=user)
         return Response({"token": token.key, "email": user.email, "user_id": user.pk})
