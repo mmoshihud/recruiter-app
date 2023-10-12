4884cb8ab057fc8f582376abdbd750aace30d4d0
Munshi Moshihud Hossain <63121629+mmoshihud@users.noreply.github.com>
2023-10-12 18:59:10 +0600

    notification count

--- notification/rest/serializer/notification.py
+++ notification/rest/serializer/notification.py
@@ -4,6 +4,13 @@ from notification.models import Notification
 
 
 class NotificationSerializer(serializers.ModelSerializer):
+    unread_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Notification
         fields = "__all__"
+
+    def get_unread_count(self, obj):
+        return Notification.objects.filter(
+            is_read=True, organization=obj.organization
+        ).count()
--- notification/rest/views/notification.py
+++ notification/rest/views/notification.py
@@ -1,19 +1,13 @@
 from rest_framework import generics
-from core.models import User
 from notification.models import Notification
 from notification.rest.serializer.notification import NotificationSerializer
-from organization.models import OrganizationUser
 
 
 class PrivateNotificationList(generics.ListAPIView):
-    # queryset = Notification.objects.filter(is_read=False)
     serializer_class = NotificationSerializer
 
     def get_queryset(self):
         user = self.request.user
-        # queryset = User.objects.filter()
-        # queryset = queryset.exclude(organizationuser__user=user)
         queryset = Notification.objects.filter(organization=None)
-        queryset = queryset.exclude(organization__organizationuser__user=user)
-        # notification = Notification.objects.filter(is_read=False)
-        return queryset
+        notification = queryset.exclude(organization__organizationuser__user=user)
+        return notification
